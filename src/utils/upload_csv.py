fb519080b28d1e22a066ff6b9e2c9065116e0fd7
chmadran <charlotte.madrangeas@gmail.com>
2025-05-02 19:15:30 +0200

    upload_csv remove comments

--- src/utils/upload_csv.py
+++ src/utils/upload_csv.py
@@ -1,7 +1,5 @@
 import pandas as pd
 
-# la fonction read_csv prend en charge la verification du path et procede a la mise en flux comme open()
-# on peut donc se focus sur les erreurs systeme et le parsing, en utilisant directement pd.read_csv() et ses options
 def upload_csv(filepath: str) -> pd.DataFrame:
     """
     This function loads a CSV file from the given path and returns a DataFrame.
@@ -11,12 +9,10 @@ def upload_csv(filepath: str) -> pd.DataFrame:
     """
     if not filepath.endswith('.csv'):
         raise ValueError("Invalid file format. Please provide a CSV file.")
-
     try:
         data = pd.read_csv(filepath, encoding='utf-8')
         if data.empty:
             raise ValueError("The CSV file is empty.")
-        # print(f"Successfully loaded a CSV file of dimensions: {data.shape}!")
         return data
     except FileNotFoundError:
         raise FileNotFoundError(f"File not found: {filepath}")
