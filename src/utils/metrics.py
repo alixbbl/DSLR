d49824e877594cd8ceff75960b19ee67951fae9d
chmadran <charlotte.madrangeas@gmail.com>
2025-06-06 11:39:50 +0200

    clean print metric.py

--- src/utils/metrics.py
+++ src/utils/metrics.py
@@ -9,4 +9,5 @@ def calculate_accuracy(y_pred, y_true) -> float:
     correct = sum(1 for pred, true in zip(y_pred, y_true) if pred == true)
     total = len(y_true)
     accuracy = correct / total if total > 0 else 0
+    print(f"Accuracy: {accuracy * 100:.2f}%")
     return accuracy
